d8ef1fe4106946077f8be3cc056cffb55434cf0d
Hyungin Choi <01_01@naver.com>
2020-01-03 19:18:10 +0900

    Add session

--- todo/src/main/java/com/example/todo/Controller/SigninController.java
+++ todo/src/main/java/com/example/todo/Controller/SigninController.java
@@ -1,10 +1,16 @@
 package com.example.todo.Controller;
 
+import javax.servlet.http.HttpSession;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.example.todo.Model.User;
 import com.example.todo.Repository.UserRepository;
 
 import lombok.extern.slf4j.Slf4j;
@@ -12,14 +18,25 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @Controller
 public class SigninController {
-
+	
 	@Autowired
 	UserRepository userRepository;
 	
+	@Autowired
+	HttpSession session;
+
 	@GetMapping("/signin")
-	public String html() {
-		return "cus/signIn";
-		
+	public String signin() {
+		return "signin";
+	}
+
+	@PostMapping("/signin")
+	public String signinPost(@ModelAttribute User user) {
+	User dbUser = userRepository.findByNickNameAndPassWord(user.getNickName(), user.getPassWord());
+	if(dbUser != null) {
+	session.setAttribute("user_info", dbUser);
+	}
+	return "redirect:/";
 	}
 	
 	Logger logger = LoggerFactory.getLogger(this.getClass());
--- todo/src/main/java/com/example/todo/Repository/UserRepository.java
+++ todo/src/main/java/com/example/todo/Repository/UserRepository.java
@@ -5,4 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.todo.Model.User;
 
 public interface  UserRepository extends JpaRepository<User, Long>{
-}
+	public User findByNickNameAndPassWord(String nickName, String passWord);
+	}

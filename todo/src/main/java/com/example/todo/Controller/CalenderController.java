fa830a910c620b5233b641f7910f8a25294d0cfb
heesoo <mhs3458@naver.com>
2020-01-06 19:32:42 +0900

    Mod script in index.html

--- todo/src/main/java/com/example/todo/Controller/CalenderController.java
+++ todo/src/main/java/com/example/todo/Controller/CalenderController.java
@@ -4,9 +4,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.example.todo.Model.User;
 
 import lombok.extern.slf4j.Slf4j;
+
 
 @Slf4j
 @Controller
@@ -14,8 +18,14 @@ public class CalenderController {
 
 	@GetMapping("/calender")
 	public String html() {
-		return "m_calender";
+		return "calender";
 		
+	}
+	@PostMapping("/calender")
+	public String calenderPost() {
+		
+		return "calender";
+	
 	}
 	Logger logger = LoggerFactory.getLogger(this.getClass());
 }

bdacb9ba0c65010f2e4be18c40b4d31064633556
Juhyun Shin <jshinv@gmail.com>
2020-01-07 13:50:50 +0900

    Add friend area

--- todo/src/main/java/com/example/todo/Controller/SetController.java
+++ todo/src/main/java/com/example/todo/Controller/SetController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.example.todo.Repository.TodoRepository;
 import com.example.todo.Repository.UserRepository;
 
 import lombok.extern.slf4j.Slf4j;
@@ -17,6 +18,7 @@ import lombok.extern.slf4j.Slf4j;
 public class SetController {
 	@Autowired
 	UserRepository userRepository;
+	TodoRepository todoRepository;
 
 	@Autowired
 	HttpSession session;

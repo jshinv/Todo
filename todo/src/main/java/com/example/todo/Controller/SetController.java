934675e5e1da8eba8516c684d053c5580cbd15e0
Juhyun Shin <jshinv@gmail.com>
2020-01-07 13:18:14 +0900

    make button

--- todo/src/main/java/com/example/todo/Controller/SetController.java
+++ todo/src/main/java/com/example/todo/Controller/SetController.java
@@ -1,21 +1,36 @@
 package com.example.todo.Controller;
 
+import javax.servlet.http.HttpSession;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.todo.Repository.UserRepository;
 
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Controller
 public class SetController {
+	@Autowired
+	UserRepository userRepository;
 
+	@Autowired
+	HttpSession session;
+	
 	@GetMapping("/setting")
 	public String html() {
 		return "cus/setting";
 		
 	}
+	
+
+
+	
+	
+	
 	Logger logger = LoggerFactory.getLogger(this.getClass());
 }
